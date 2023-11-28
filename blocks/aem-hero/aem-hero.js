1bf1d8d0d1dbf5f5ef6b3d29992bc972c30d6187
Carlos A. Cabrera <cacabrer@adobe.com>
2023-11-28 17:57:05 -0600

    eager

--- blocks/aem-hero/aem-hero.js
+++ blocks/aem-hero/aem-hero.js
@@ -1,21 +1,20 @@
 import { Block } from '../../scripts/aem-blocks.js';
 
 export default class Hero extends Block {
-  image;
-
-  heading;
-
   constructor() {
     super();
 
     const item = this.querySelector('div[slot="item"]');
-    this.image = item.querySelector('aem-image');
-    this.heading = item.querySelector('p + *');
+    const image = item.querySelector('aem-image');
+    const heading = item.querySelector('p + *');
     this.innerHTML = '';
-  }
 
-  connectedCallback() {
-    this.shadowRoot.querySelector('slot[name="image"]').append(this.image);
-    this.shadowRoot.querySelector('slot[name="heading"]').append(this.heading);
+    image.querySelectorAll('img').forEach((img) => {
+      // Eager
+      img.setAttribute('loading', 'eager');
+    });
+
+    this.shadowRoot.querySelector('slot[name="image"]').append(image);
+    this.shadowRoot.querySelector('slot[name="heading"]').append(heading);
   }
 }
