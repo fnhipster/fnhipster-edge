f8932b3f7bd169ea2b0becac3deec88689b47926
Carlos A. Cabrera <cacabrer@adobe.com>
2023-12-14 17:14:03 -0600

    test

--- aem.config.js
+++ aem.config.js
@@ -1,19 +1,18 @@
 export default {
   scripts: [
-    {
-      route: /.*/,
-      path: '/vendors/fn-ui/components/index.js',
-      // path: 'http://localhost:6006/components/index.js',
-      eager: true,
-    },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-app/fn-app.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-binary/fn-binary.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-content/fn-content.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-footer/fn-footer.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-header/fn-header.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-image/fn-image.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-link/fn-link.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-logo/fn-logo.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-columns/fn-columns.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-hero/fn-hero.js', eager: true },
   ],
   styles: [
-    {
-      route: /.*/,
-      path: '/vendors/fn-ui/styles/fn.css',
-      // path: 'http://localhost:6006/styles/fn.css',
-      eager: true,
-    },
+    { route: /.*/, path: '/vendors/fn-ui/styles/fn.css', eager: true },
     // {
     //   route: /.*/,
     //   path: 'https://use.typekit.net/joe8rol.css',
