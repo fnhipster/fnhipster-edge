4a65298fd27eaff6e649e3a03ff481f572ad39ba
Carlos A. Cabrera <cacabrer@adobe.com>
2023-12-14 17:21:48 -0600

    test

--- aem.config.js
+++ aem.config.js
@@ -1,14 +1,14 @@
 export default {
   scripts: [
     { route: /.*/, path: '/vendors/fn-ui/components/fn-app/fn-app.js', eager: true },
-    // { route: /.*/, path: '/vendors/fn-ui/components/fn-binary/fn-binary.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-binary/fn-binary.js', eager: true },
     { route: /.*/, path: '/vendors/fn-ui/components/fn-content/fn-content.js', eager: true },
-    // { route: /.*/, path: '/vendors/fn-ui/components/fn-footer/fn-footer.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-footer/fn-footer.js', eager: true },
     { route: /.*/, path: '/vendors/fn-ui/components/fn-header/fn-header.js', eager: true },
     { route: /.*/, path: '/vendors/fn-ui/components/fn-image/fn-image.js', eager: true },
     { route: /.*/, path: '/vendors/fn-ui/components/fn-link/fn-link.js', eager: true },
-    // { route: /.*/, path: '/vendors/fn-ui/components/fn-logo/fn-logo.js', eager: true },
-    // { route: /.*/, path: '/vendors/fn-ui/components/fn-columns/fn-columns.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-logo/fn-logo.js', eager: true },
+    { route: /.*/, path: '/vendors/fn-ui/components/fn-columns/fn-columns.js', eager: true },
     { route: /.*/, path: '/vendors/fn-ui/components/fn-hero/fn-hero.js', eager: true },
   ],
   styles: [
